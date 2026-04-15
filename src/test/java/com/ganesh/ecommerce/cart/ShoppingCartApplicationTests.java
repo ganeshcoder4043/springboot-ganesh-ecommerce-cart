6ceaecb321bab7d2e11d09312d98c54766665fd3
ganeshcoder4043 <ganesha4043@gmail.com>
2026-04-15 15:39:17 +0530

    first commit and file structure make and dependecy add

---
+++ src/test/java/com/ganesh/ecommerce/cart/ShoppingCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ganesh.ecommerce.cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
